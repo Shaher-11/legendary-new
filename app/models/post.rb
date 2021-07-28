491b312e1079fa3db89a58ef0f9097b9f87209ab
Shaher-11 <shahershamroukh@gmail.com>
2021-07-28 23:38:29 +0200

    Modify Element cotroller and model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :author
+  has_many :elements
   scope :popular, -> { select('post_id, count(post_id) as count').group(:post_id).order('count desc').limit(10) }
 end
