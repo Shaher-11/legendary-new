71e9baf3c2e1bae141db7890f6f4ccdc1fa34530
Shaher-11 <shahershamroukh@gmail.com>
2021-07-28 23:00:17 +0200

    Scaffold element

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
   scope module: 'authors' do
-    resources :posts
+    resources :posts do
+      resources :elements
+    end
   end
 end
