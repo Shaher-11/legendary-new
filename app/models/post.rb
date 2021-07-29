aafb6a5aa4ba31a268ca27b95b564daa1533b269
Shaher-11 <shahershamroukh@gmail.com>
2021-07-29 12:36:30 +0200

    Implement image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author
   has_many :elements
+
+  has_one_attached :header_image
   scope :popular, -> { select('post_id, count(post_id) as count').group(:post_id).order('count desc').limit(10) }
 end
